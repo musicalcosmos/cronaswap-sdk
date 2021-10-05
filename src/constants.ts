ef792f64f022ae3828ab9639fb02dd4d9cfb2b05
CronaChef <cronaswap@protonmail.com>
2021-10-05 16:27:02 +0800

    support cassini

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ import JSBI from 'jsbi'
 export type BigintIsh = JSBI | bigint | string
 
 export enum ChainId {
-  MAINNET = 56,
-  TESTNET = 338
+  MAINNET = 339,
+  TESTNET = 338,
 }
 
 export enum TradeType {
